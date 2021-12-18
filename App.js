802ec3d419c882030447f7d83c379af4d21ec3af
unknown <dibyasundar444@gmail.com>
2021-12-19 02:00:46 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Button } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CameraScreen from "./src/CameraScreen";
+
+
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Capture" onPress={()=>navigation.navigate("Camera")} />
+    </View>
+  )
+};
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+      </Stack.Navigator>
+  </NavigationContainer>
+  );
+};
